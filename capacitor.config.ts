04e97da36c0002c614f053e6c2e7c6fab06d7d48
danieledelia <danieledelia.freelance@gmail.com>
2025-09-07 11:39:23 +0000

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tmp',
+  webDir: 'www'
+};
+
+export default config;
